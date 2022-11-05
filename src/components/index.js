fda63d997bf0bf3f69d9a2cf85f95875e86fa9cf
chushuang <1214106564@qq.com>
2022-11-05 12:13:34 +0800

    ADD：打印+pdf下载

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,8 @@ import PageTools from './PageTools'
 import UploadExcel from './UploadExcel'
 import ScreenFull from './ScreenFull'
 import ImageUpload from './ImageUpload'
+import Print from 'vue-print-nb'
+import vueToPdf from 'vue-to-pdf'
 
 export default {
   install(Vue) {
@@ -11,5 +13,9 @@ export default {
     Vue.component('UploadExcel', UploadExcel)
     Vue.component('ScreenFull', ScreenFull)
     Vue.component('ImageUpload', ImageUpload)
+    // 注册好打印插件，会有一个 v-print 指令的方式进行打印
+    Vue.use(Print)
+    // dom结构生成pdf的插件
+    Vue.use(vueToPdf)
   }
 }
--- src/views/employees/detail.vue
+++ src/views/employees/detail.vue
@@ -20,12 +20,26 @@
 
           <!-- 个人详情 -->
           <el-tab-pane label="个人详情">
+             <el-row type="flex" justify="end">
+              <el-tooltip content="打印个人基本信息">
+                  <router-link :to="`/employees/print/${userId}?type=personal`">
+                    <i class="el-icon-printer" />
+                  </router-link>
+              </el-tooltip>
+            </el-row>
             <!-- 放置个人详情 -->
             <component :is="UserComponent" />
           </el-tab-pane>
 
           <!-- 岗位信息 -->
           <el-tab-pane label="岗位信息">
+            <el-row type="flex" justify="end">
+              <el-tooltip content="打印岗位信息">
+                <router-link :to="`/employees/print/${userId}?type=job`">
+                  <i class="el-icon-printer"  />
+                </router-link>
+              </el-tooltip>
+            </el-row>
             <!-- 放置岗位详情 -->
             <component :is="JobInfo" />
           </el-tab-pane>
